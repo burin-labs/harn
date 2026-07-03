edb78a5a3d356fd7054e47f52c18af1fd5d4ce68
Kenneth Sinder <kennethsinder@gmail.com>
2026-07-02 17:01:32 -0700

    dev: reuse hook cargo targets

--- scripts/check_no_rust_prompt_prose.sh
+++ scripts/check_no_rust_prompt_prose.sh
@@ -1,5 +1,22 @@
 #!/bin/sh
 set -eu
 
-cargo run --quiet --bin harn -- run scripts/check_rust_prompt_prose.harn -- --self-test
-cargo run --quiet --bin harn -- run scripts/check_rust_prompt_prose.harn
+resolve_harn_bin() {
+  if [ -n "${HARN_BIN:-}" ]; then
+    printf '%s\n' "$HARN_BIN"
+    return
+  fi
+
+  cargo build --quiet --bin harn
+  target_dir="$(cargo metadata --format-version=1 --no-deps \
+    | python3 -c 'import json, sys; print(json.load(sys.stdin)["target_directory"])')"
+  suffix=""
+  case "$(uname -s)" in
+    Windows_NT|MINGW*|MSYS*|CYGWIN*) suffix=".exe" ;;
+  esac
+  printf '%s/debug/harn%s\n' "$target_dir" "$suffix"
+}
+
+harn_bin="$(resolve_harn_bin)"
+"$harn_bin" run scripts/check_rust_prompt_prose.harn -- --self-test
+"$harn_bin" run scripts/check_rust_prompt_prose.harn
